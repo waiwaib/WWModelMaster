fead2eb8614b493bbfc7c2a7633dd26d97da63a1
waiwaib <857557118@qq.com>
2015-11-05 11:05:44 +0800

    add a file WWModel.h

---
+++ WWModelMaster/Model/WWModel.h
@@ -0,0 +1,15 @@
+//
+//  WWModel.h
+//  WWModelMaster
+//
+//  Created by 歪歪 on 15/11/5.
+//  Copyright © 2015年 歪歪. All rights reserved.
+//
+
+#ifndef WWModel_h
+#define WWModel_h
+
+#import "Model.h"
+#import "modelAssociateDB.h"
+
+#endif /* WWModel_h */
